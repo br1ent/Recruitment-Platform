3d9676217e6925df98135c4062d8694158f0c27a
石铭轩 <530220399@qq.com>
2026-07-05 09:14:04 +0800

    完成登录注册页面

--- frontend/src/views/user/LoginView.vue
+++ frontend/src/views/user/LoginView.vue
@@ -3,8 +3,22 @@
 </script>
 
 <template>
-  <div>
-    登录
+  <div class="flex justify-center align-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend text-base">登录</legend>
+
+      <label class="label text-base">邮箱</label>
+      <input type="email" class="input"/>
+
+      <label class="label text-base">密码</label>
+      <input type="password" class="input" />
+
+      <button class="btn btn-neutral mt-4">登录</button>
+      <div class="mt-2">
+        <span class="text-sm">还没有账号？点击</span>
+        <router-link :to="{name: 'register-index'}" class="link link-hover text-sm">注册</router-link>
+      </div>
+    </fieldset>
   </div>
 </template>
 
--- frontend/src/views/user/RegisterView.vue
+++ frontend/src/views/user/RegisterView.vue
@@ -3,8 +3,28 @@
 </script>
 
 <template>
-  <div>
-    注册
+  <div class="flex justify-center align-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend text-base">注册</legend>
+
+      <label class="label text-base">用户名</label>
+      <input type="email" class="input"/>
+
+      <label class="label text-base">邮箱</label>
+      <input type="email" class="input"/>
+
+      <label class="label text-base">密码</label>
+      <input type="password" class="input" />
+
+      <label class="label text-base">确认密码</label>
+      <input type="password" class="input" />
+
+      <button class="btn btn-neutral mt-4">注册</button>
+      <div class="mt-2">
+        <span class="text-sm">已经有账号了？点击</span>
+        <router-link :to="{name: 'login-index'}" class="link link-hover text-sm">返回登录</router-link>
+      </div>
+    </fieldset>
   </div>
 </template>
 
